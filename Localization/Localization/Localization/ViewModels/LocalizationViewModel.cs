61f2ceb9a84f6b881d802aac675c4de88235a61f
Omar <ojoyar@gmail.com>
2017-03-05 16:13:55 -0600

    INotifyPropertyChanged implemented

---
+++ Localization/Localization/Localization/ViewModels/LocalizationViewModel.cs
@@ -0,0 +1,43 @@
+﻿using Xamarin.Forms;
+
+namespace Localization.ViewModels
+{
+    using System;
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+
+
+    public class LocalizationViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+    
+        public readonly ContentPage View;
+        public Command BtnConnect { get; set; }
+        public LocalizationViewModel(ContentPage view)
+        {
+            this.View = view;
+            Connected = false;
+            BtnConnect = new Command(Connect);
+        }
+        private bool connected;
+        public bool Connected
+        {
+            get { return connected; }
+            set
+            {
+                connected = value;
+                OnPropertyChanged();
+            }
+        }
+
+        void Connect()
+        {
+            Connected = !Connected;
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Localization/Localization/Localization/Views/Localization.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace Localization
+{
+    using System;
+    using Localization.ViewModels;
+    using Xamarin.Forms;
+
+    public partial class MainPage : ContentPage
+    {
+        private LocalizationViewModel vm;
+        public MainPage()
+        {
+            InitializeComponent();
+            vm= new LocalizationViewModel(this);
+            BindingContext = vm;
+        }
+    }
+}
